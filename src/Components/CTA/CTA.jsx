e00b625319846776298841a0861b1de3815f15bf
phuchs47203 <21522479@gm.uit.edu.vn>
2023-10-28 23:28:47 +0700

    check email before send

--- src/Components/CTA/CTA.jsx
+++ src/Components/CTA/CTA.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CTA.css'
 import { AiOutlineSend } from 'react-icons/ai';
 const CTA = () => {
+  const [erroEmail, seterroEmail] = useState("");
+  const [successEmail, setsuccessEmail] = useState("");
+  const [textEmail, settextEmail] = useState("");
+  const handleChangeEmail = (e) => {
+    const value = e.target.value;
+    settextEmail(value);
+    setsuccessEmail('');
+  }
+  const handleSendEmail = (e) => {
+    e.preventDefault();
+    setsuccessEmail('');
+    if (!textEmail) {
+      seterroEmail('Email address cannot be empty');
+      return;
+    }
+    seterroEmail('');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailPattern.test(textEmail)) {
+      seterroEmail('Email is not valid. Ex-name@gmail.com');
+      return;
+    }
+    settextEmail("");
+    seterroEmail("");
+    setsuccessEmail('Successfull');
+    return;
+  };
   return (
     <div className='app-helmerts-cta'>
       <div className='app-helmerts-cta-header'>
@@ -10,8 +36,26 @@ const CTA = () => {
         </h1>
       </div>
       <div className='app-helmerts-cta-content'>
-        <input type="text" placeholder='Email address' />
-        <AiOutlineSend />
+        <input
+          type="text"
+          value={textEmail}
+          onChange={(e) => handleChangeEmail(e)}
+          placeholder='Email address' />
+        <AiOutlineSend onClick={(e) => handleSendEmail(e)} />
+      </div>
+      <div className='app-helmerts-cta-error'>
+        {erroEmail
+          &&
+          <p
+            className="error-message">{erroEmail}
+          </p>}
+      </div>
+      <div className='app-helmerts-cta-error'>
+        {successEmail
+          &&
+          <p
+            className="success-message">{successEmail}
+          </p>}
       </div>
     </div>
   )
